e87178105b053cc3271f1f8f4064cf24abb1f6ae
seochanhyeok <mushroom1324@naver.com>
2023-02-05 15:07:24 +0900

    spring security

---
+++ src/main/java/com/example/security1/controller/IndexController.java
@@ -0,0 +1,46 @@
+package com.example.security1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller // returns View
+public class IndexController {
+
+    @GetMapping({"", "/"})
+    public String index() {
+        // using Mustache
+        // .mustache
+        return "index"; // src/main/resources/templates/index.mustache
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody String user() {
+        return "user";
+    }
+
+    @GetMapping("/adin")
+    public @ResponseBody String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody String manager() {
+        return "manager";
+    }
+
+    @GetMapping("/login")
+    public @ResponseBody String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public @ResponseBody String join() {
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc() {
+        return "register completed!";
+    }
+}
